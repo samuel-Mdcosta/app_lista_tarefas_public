777c053cccd9d4e01b9668307f0053a2dd07c81c
SAMUEL MARQUES DA COSTA <costadev19@gmail.com>
2025-01-11 00:28:37 -0400

    2.0 listar tarefas
    inclusão da listagens das tarefas e inclusão das tarefas no banco de dados

--- nova_tarefa.php
+++ nova_tarefa.php
@@ -19,6 +19,12 @@
 			</div>
 		</nav>
 
+		<?php if(isset($_GET['inclusao']) && $_GET['inclusao'] == 1) { ?>
+			<div class="bg-success pt-2 text-white d-flex justify-content-center">
+				<h5>Tarefa inserida com sucesso</h5>
+			</div>
+		<?php } ?>
+
 		<div class="container app">
 			<div class="row">
 				<div class="col-md-3 menu">
@@ -36,7 +42,7 @@
 								<h4>Nova tarefa</h4>
 								<hr />
 
-								<form method="post" action="tarefa_controler.php">
+								<form method="post" action="tarefa_controler.php?acao=inserir">
 									<div class="form-group">
 										<label>Descrição da tarefa:</label>
 										<input type="text" class="form-control" name="tarefa" placeholder="Exemplo: Lavar o carro">
